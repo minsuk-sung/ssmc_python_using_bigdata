fd1a959959084362f44ef4856136abeac56a592e
mssung94 <mssung94@gmail.com>
2019-01-02 09:59:30 +0900

    game finish

--- week1/day3/p13_game_ans.py
+++ week1/day3/p13_game_ans.py
@@ -80,35 +80,77 @@ print(game_info)
 # 숫자가 아닌 값을 넣으면 '경고 메세지' 다시 입력 -> isnumeric()
 # 0보다 작거나, 100 이상의 값을 입력하면 '경고메세지' 다시 입력 -> or 쓰자
 # 정상 범위에 있는 정수값을 입력하면 다음 단계 진행
-while True:
-    # 공백 제거를 해서 오동작을 방지
-    game_input = input('AI의 숫자(0~99)를 입력하세요.\n').strip()
-    if not game_input: # 숫자를 넣지 않으면
-        print('입력을 정확하게 하세요')
-    elif not game_input.isnumeric(): #숫자가 아닌 값을 넣으면
-        print('0~99사이의 정수값을 입력하세요')
-    else:
-        tmp = int(game_input)
-        if tmp >= 0 and tmp < 100:
+# 게임 시도 횟수를 저장하는 변수
+game_on = True
+while game_on: # 게임 전체를 반복하기 위해서
+    ai_value = 0
+    try_count = 0
+    while True: # 사용자가 AI값을 맞출때까지 반복
+        while True: # 사용자가 숫자를 입력하는 반복
+            # 공백 제거를 해서 오동작을 방지
+            game_input = input('AI의 숫자(0~99)를 입력하세요.\n').strip()
+            if not game_input: # 숫자를 넣지 않으면
+                print('입력을 정확하게 하세요')
+            elif not game_input.isnumeric(): #숫자가 아닌 값을 넣으면
+                print('0~99사이의 정수값을 입력하세요')
+            else:
+                game_input = int(game_input)
+                if game_input >= 0 and game_input < 100:
+                    break
+                print('적절한 정수값을 입력하세요')
+        try_count += 1 # try_count = try_count + 1
+        print('{0}번 시도 했습니다.'.format(try_count))
+
+
+        # STEP5
+        # AI는 숫자를 하나 생성한다 ( 랜덤 ) -> 1회만 생성
+        # 즉 게임 한번이 종료될때까지 유지되어야 한다
+        # 모듈 가져오기 ( 다른 사람이 만든 라이브러리 이해 )
+        # 파이썬은 모듈,패키지 밖에 없다 
+        import random
+        if not ai_value: # ai 값이 세팅되어 있지 않았다면 (딱 한번만 세팅해주기 위해서)
+            ai_value = random.randint(0,99) # 그럴 경우에는 세팅해라
+            print("AI : {0}".format(ai_value))
+
+        # STEP6
+        # 판단
+        # 1) 입력값이 정답보다 작으면 => 작다라고 출력!
+        # 2) 입력값이 정답보다 크면 => 크다고 출력!
+        # 3) 입력값이 정답과 동일하면 => 정답! 축하메세지! => STEP7
+        if game_input < ai_value:
+            print('입력값이 작습니다')
+        elif game_input > ai_value:
+            print('입력값이 큽니다')
+        else:
+            print('정답입니다!')
             break
-        print('적절한 정수값을 입력하세요')
-print('한번 시도 했습니다.')
+            
 
-# STEP5
-# AI는 숫자를 하나 생성한다 ( 랜덤 ) -> 1회만 생성
-# 즉 게임 한번이 종료될때까지 유지되어야 한다
+    # STEP7
+    # 결과 표시
+    # 총 시도 횟수, 점수(10-총시도횟수)*10를 표시
+    # 다시 게임을 하시겠습니까?
+    # YES (대소문자 구분안함) -> STEP4으로
+    # NO (대소문자 구분안함) -> 프로그램 종료
+    # 아무것도 입력하지 않아도 경고 메세지
+    # 입력값이 틀려도 경고 메세지
+    prompt = ''' 
 
-# STEP6
-# 판단
-# 1) 입력값이 정답보다 작으면 => 작다라고 출력!
-# 2) 입력값이 정답보다 크면 => 크다고 출력!
-# 3) 입력값이 정답과 동일하면 => 정답! 축하메세지!
+총 시도 횟수 : %s
+점수 : %s
+다시 게임하시겠습니까?(YES/NO)
 
-# STEP7
-# 결과 표시
-# 총 시도 횟수, 점수(10-총시도횟수)*10를 표시
-# 다시 게임을 하시겠습니까?
-# YES (대소문자 구분안함) -> STEP4으로
-# NO (대소문자 구분안함) -> 프로그램 종료
-# 아무것도 입력하지 않아도 경고 메세지
-# 입력값이 틀려도 경고 메세지  
+''' % (try_count,(10-try_count)*10)
+    while True:
+        result = input(prompt).strip().upper()
+        if not result:
+            print('YES / NO 중 하나를 입력하세요')
+        elif result == 'NO':
+            print('Bye Bye!')
+            game_on = False
+            break
+        elif result == 'YES':
+            print('Game Restart')
+            break
+        else:
+            print('제대로 입력해주세요')
