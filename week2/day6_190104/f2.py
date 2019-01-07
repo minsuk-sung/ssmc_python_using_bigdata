fbfbb7fdcd57212083daa0a56883dde2dd2e05bc
mssung94 <mssung94@gmail.com>
2019-01-07 11:41:10 +0900

    change hello world3

--- week2/day6_190104/f2.py
+++ week2/day6_190104/f2.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello world3'
+    return 'hello world'
 
 # 이 코드를 메인으로 구동시 서버가동
 if __name__ == '__main__':
     app.run(debug=True) 
-
