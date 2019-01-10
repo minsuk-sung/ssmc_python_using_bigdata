7d984026d2c516a209a0d1afbf8e5dc1bb44e449
mssung94 <mssung94@gmail.com>
2019-01-10 15:04:38 +0900

    login,logout 을 위한 session 처리

--- flask_advance/run.py
+++ flask_advance/run.py
@@ -12,9 +12,14 @@
     4. 세션,쿠키 처리 ( 로그인 업그레이드 )
 '''  
 
-from flask import Flask,render_template,url_for,request,redirect
+from flask import Flask,render_template,url_for,request,redirect,session
+from db.sql import *
 
 app = Flask(__name__)
+app.secret_key = 'aslkdfjklsdsdflkjs'
+
+# 세션 생성, 해체, 체크
+
 
 config = {
     'site_title' : '점심 메뉴 분석',
@@ -24,44 +29,51 @@ config = {
 
 @app.route('/')
 def home():
+    #print('home')
+    # 세션이 없으면 로그인으로
+    if not 'user_id' in session:
+        return redirect(url_for('login'))
     return render_template('index.html',config=config)
 
 @app.route('/logout')
 def logout():
     # 세션 제거
+    if 'user_id' in session:
+        session.pop('user_id',None) # del session['user_id'] 라고 해도 됨
+    if 'user_nm' in session:
+        session.pop('user_nm',None)
+
     # 홈페이지 이동 
-    return render_template('logout.html',config=config)
+    # return render_template('logout.html',config=config)
+    return redirect(url_for('login'))
 
 # restful 방식
 # get방식과 post방식을 모두 허용하는 라우트 정의  
 @app.route('/login',methods=['GET','POST'])
 def login():
 
-    # GET 방식일 경우
+    # GET 방식
     if request.method == 'GET':
         return render_template('login.html',config=config)
 
-    # # POST 방식일 경우 => methods에는 2개밖에 없으므로 else
-    # else :
-    #     # 1. 아이디 비번을 획득하고
-    #     uid = request.form.get('uid')
-    #     upw = request.form['upw']
-    #     # 1-1 누락된 값이 존재할 경우, 경고 후 되돌린다 
-    #     if not uid or not upw: # 아이디나 비번이 없는 경우
-    #         return render_template('error.html',msg = '비정상적인 접근입니다.')
+    # POST 방식
+    else :
+        uid = request.form['uid']
+        upw = request.form['upw']
+        row = loginSql(uid,upw)
+        print( row, uid, upw )
+        if row:
+            # 세션 생성
+            #   클라이언트 정보를 서버가 유지하거나 보관
+            #   단, 서버가 들고 있으면 리소스가 부족해지거나 성능 저하를 가져올 수 있다
+            #   실습할때는 서버 메모리에 들고 있을 것이고, 서비스할때는 디비나 서드파트 솔루션 이용하여 처리
+            session['user_id'] = uid
+            session['user_nm'] = row['name']
 
-    #     # 2. 우리 회원인지 조회하고 ( 차후 DB 조회 )
-    #     print(uid,upw)
-
-    #     # 3. 회원이면 서비스로 이동
-    #     if uid == 'm' and upw == '1':
-    #         # 3-1. 세션 생성
-    #         # 3-2. 페이지 이동 ( 요청을 포워딩 ) 
-    #         return redirect(url_for('main'))
-
-    #     # 4. 회원 아니면 경고 후 페이지를 되돌린다
-    #     else :
-    #         return render_template('error.html',msg = '아이디 혹은 비밀번호를 확인해주세요.')
+            # 홈페이지 이동 
+            return redirect(url_for('home'))
+        else :
+            return render_template('error.html',msg = '아이디 혹은 비밀번호를 확인해주세요.')
 
 # 이 코드를 메인으로 구동시 서버가동
 if __name__ == '__main__':
